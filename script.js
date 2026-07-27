e8a21ac61488f1ea69fa26292a162b2783a41d54
Dlopez2910 <dportilho54@gmail.com>
2026-07-27 14:49:37 -0400

    adicionado funcionalidade de exibição e ocultação do formulário de contato com animações

---
+++ script.js
@@ -0,0 +1,21 @@
+const formulario = document.querySelector(".formulario");
+const mascara = document.querySelector(".mascara-formulario")
+
+
+
+
+
+function mostrarFormulario() {
+    formulario.style.left = "50%";
+    formulario.style.transform = "translateX(-50%)";
+    mascara.style.visibility = "visible";
+
+}
+
+
+function esconderFormulario() {
+    formulario.style.left = "-340px";
+    formulario.style.transform = "translateX(0)";
+    mascara.style.visibility = "hidden";
+
+}
